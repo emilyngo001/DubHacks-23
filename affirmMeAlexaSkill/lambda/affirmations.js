30463ffb7173f3f37afbab003c47923882aa3142
emilyngo001 <51425169+emilyngo001@users.noreply.github.com>
2023-10-15 11:26:03 -0700

    added Alexa Skill code

---
+++ affirmMeAlexaSkill/lambda/affirmations.js
@@ -0,0 +1,14 @@
+module.exports = [
+    'I am successful.', 
+    'I am strong.',
+    'I am living with abundance.',
+    'I am inspiring people through my work.',
+    'Today is a phenomenal day.',
+    'I am independent and self-sufficient.',
+    'Today will be a productive day.',
+    'Each and every day, I am getting closer to achieving my goals.',
+    'My actions are meaningful and inspiring.',
+    'I am intelligent and focused.',
+    'I am not pushed by my problems; I am led by my dreams.',
+    'I am filled with focus.',
+    ];
